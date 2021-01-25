37f9b8c69d662a362a557793bad0f5e1122f1d02
LeviJunior12 <levicoc123@gmail.com>
2021-01-24 21:12:55 -0300

    Feat register screen

--- app/src/main/java/com/levi/desafio4/ui/RegisterFragment.kt
+++ app/src/main/java/com/levi/desafio4/ui/RegisterFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.levi.desafio4.R
-import kotlinx.android.synthetic.main.login_body.view.*
 
-class LoginFragment : Fragment() {
+
+class RegisterFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +18,8 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
-        registerUser(view)
+        var view = inflater.inflate(R.layout.fragment_register, container, false)
         return view
     }
 
-    private fun registerUser(view: View) {
-        view.tv_create_account.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
-    }
 }
