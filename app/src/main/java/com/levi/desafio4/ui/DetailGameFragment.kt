97ff9a524350e35bee24f41f8f51610272bcd716
LeviJunior12 <levicoc123@gmail.com>
2021-01-25 14:40:54 -0300

    Feat detail game screen

---
+++ app/src/main/java/com/levi/desafio4/ui/DetailGameFragment.kt
@@ -0,0 +1,24 @@
+package com.levi.desafio4.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.levi.desafio4.R
+
+class DetailGameFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_detail_game, container, false)
+        return view
+    }
+
+}
